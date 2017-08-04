1889900100e55e9bccc9521e1fced2f94fa0543f
Aldo Oliveira <aldovrando.oliveira@gmail.com>
2017-08-04 00:36:58 -0300

    Inclusão do contrato de repositório de UnitOfWork

---
+++ Severino.Common/Repository/IRepository.cs
@@ -0,0 +1,35 @@
+using System.Threading.Tasks;
+using Severino.Common.Domain;
+
+namespace Severino.Common.Repository
+{
+    /// <summary>
+    /// Contrato padrão para repositórios
+    /// </summary>
+    public interface IRepository<T> where T : BaseEntity
+    {
+        /// <summary>
+        /// Insere um novo item na base de dados
+        /// </summary>
+        /// <param name="item">Item a ser inserido</param>
+        Task<T> AddAsync(T item);
+
+        /// <summary>
+        /// Atualiza o item na base de dados
+        /// </summary>
+        /// <param name="item">Item atualizado</param>
+        Task UpdateAsync(T item);
+
+        /// <summary>
+        /// Remove o item da base de dados
+        /// </summary>
+        /// <param name="item">Item removido</param>
+        Task RemoveAsync(T item);
+
+        /// <summary>
+        /// Realiza a consulta de um item pelo identificador
+        /// </summary>
+        /// <param name="id">Identificador para a pesquisa</param>
+        Task<T> FindByIdAsync(long id);
+    }
+}
---
+++ Severino.Common/Repository/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace Severino.Common.Repository
+{
+    /// <summary>
+    /// Contrato para utilização do padrão 'Unit of Work'
+    /// </summary>
+    public interface IUnitOfWork
+    {
+        /// <summary>
+        /// Realiza a consolidação das alterações no banco de dados
+        /// </summary>
+        /// <returns></returns>
+        Task CommitAsync();
+    }
+}
